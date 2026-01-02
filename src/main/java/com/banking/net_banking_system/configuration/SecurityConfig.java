557510f6fde0f5e3e36f5ed6418642c860a6be86
Harsh10221 <bot54231h@gmail.com>
2026-01-02 20:40:29 +0530

    Added some dependencies

--- src/main/java/com/banking/net_banking_system/configuration/SecurityConfig.java
+++ src/main/java/com/banking/net_banking_system/configuration/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.banking.net_banking_system.configuration;
 
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +15,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
             .csrf(csrf -> csrf.disable()) // Disable CSRF for Postman testing
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/api/onboarding/**","/api/auth/**","/api/transaction/**","api/**").permitAll() // Allow these endpoints
                 .anyRequest().authenticated() // Protect everything else
             );
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
