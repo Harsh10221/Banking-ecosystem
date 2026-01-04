860512f87d4d7f87f2e808db39fdfb0935e16abd
shubh-1119 <shubhdarshanrathod@gmail.com>
2026-01-04 19:37:31 +0530

    UI update

--- bank/src/main/java/com/banking/net_banking_system/repository/TransactionRepository.java
+++ bank/src/main/java/com/banking/net_banking_system/repository/TransactionRepository.java
@@ -1,9 +1,18 @@
 package com.banking.net_banking_system.repository;
 
 import com.banking.net_banking_system.model.Transaction;
+import com.banking.net_banking_system.model.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+	
+	List<Transaction> findTop5ByUserOrderByCreatedAtDesc(User user);
+	
+	List<Transaction> findByUserOrderByCreatedAtDesc(User user);
+	
 }
